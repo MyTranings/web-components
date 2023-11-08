bfdc1fa223070fc1f86a28f1b180ffbdf35fc24d
pixaily <danield.georgiev@gmail.com>
2023-11-08 15:40:27 +0200

    Web Components - Add styles for the tooltip

--- 03.basics/tooltip.js
+++ 03.basics/tooltip.js
@@ -7,7 +7,7 @@ class Tooltip extends HTMLElement {
 
   connectedCallback() {
     const tooltipIcon = document.createElement('span');
-    
+
     if(this.hasAttribute('text')) {
       this._tooltipText = this.getAttribute('text');
     }
@@ -18,11 +18,17 @@ class Tooltip extends HTMLElement {
     tooltipIcon.addEventListener('mouseleave', this._hideTooltip.bind(this));
 
     this.appendChild(tooltipIcon);
+    this.style.position = 'relative';
   }
 
   _showTooltip() {
     this._tooltipContainer = document.createElement('div');
     this._tooltipContainer.textContent = this._tooltipText;
+    this._tooltipContainer.style.backgroundColor = 'black';
+    this._tooltipContainer.style.color = 'white';
+    this._tooltipContainer.style.position = 'absolute';
+    this._tooltipContainer.style.zIndex = '10'; 
+    this._tooltipContainer.style.backgroundColor = 'black';
     this.appendChild(this._tooltipContainer);
   }
 
